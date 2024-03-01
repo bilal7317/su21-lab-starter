1b5bf2fd2eaeb3ba5b6a7b51ad3b958df89d6627
bilal7317 <muhammadbilal7317@gmail.com>
2024-03-01 10:08:16 +0500

    Exercise 2 is done

--- lab01/exercise2/pwd_checker.c
+++ lab01/exercise2/pwd_checker.c
@@ -2,94 +2,69 @@
 #include "pwd_checker.h"
 #include <assert.h>
 
-/*
-Password checker
-
-Requirements:
-- Password must be at least 10 characters
-- Password must contain at least
-    - 1 upper case letter
-    - 1 lower case letter
-    - 1 number
-- Password cannot contain the person's first name or last name (case sensitive)
-
-For the simplicity of this exercise:
-- This is not the most efficient way to implement this program
-- These functions do not perform any error checking
-- You can assume that the first and last name will never be the empty string
-*/
-
-/* Returns true if the length of PASSWORD is at least 10, false otherwise */
+// Checks if the length of the password is at least 10
 bool check_length(const char *password) {
-    int length = strlen(password);
-    bool meets_len_req = (length >= 10);
-    return meets_len_req;
+    int length = strlen(password); // Get the length of the password
+    bool meets_len_req = (length >= 10); // Check if the length is at least 10
+    return meets_len_req; 
 }
 
-/* Returns true if LETTER is in the range [LOWER, UPPER], false otherwise */
+// Checks if a character is within a specified range
 bool check_range(char letter, char lower, char upper) {
-    bool is_in_range = (letter >= lower && letter <= upper);
-    return is_in_range;
+    bool is_in_range = (letter >= lower && letter <= upper); // Check if the letter is within the range
+    return is_in_range; 
 }
 
-/* Returns true if PASSWORD contains at least one upper case letter, false otherwise */
+// Checks if the password contains at least one uppercase letter
 bool check_upper(const char *password) {
-    while (*password != '\0') {
-        bool is_in_range = check_range(*password, 'A', 'Z');
-        if (is_in_range) {
-            return true;
+    while (*password != '\0') { // Loop through each character in the password
+        bool is_in_range = check_range(*password, 'A', 'Z'); // Check if the character is an uppercase letter
+        if (is_in_range) { // If an uppercase letter is found
+            return true; 
         }
-        ++password;
+        ++password; // Move to the next character
     }
-    return false;
+    return false; 
 }
 
-/* Returns true if PASSWORD contains at least one lower case letter, false otherwise */
+// Checks if the password contains at least one lowercase letter
 bool check_lower(const char *password) {
-    while (*password != '\0') {
-        bool is_in_range = check_range(*password, 'a', 'z');
-        if (is_in_range) {
-            return true;
+    while (*password != '\0') { // Loop through each character in the password
+        bool is_in_range = check_range(*password, 'a', 'z'); // Check if the character is a lowercase letter
+        if (is_in_range) { // If a lowercase letter is found
+            return true; 
         }
-        ++password;
+        ++password; // Move to the next character
     }
-    return false;
+    return false; 
 }
 
-/* Returns true if PASSWORD contains at least one number, false otherwise */
+// Checks if the password contains at least one number
 bool check_number(const char *password) {
-    while (*password != '\0') {
-        if (check_range(*password, 49, 58)) {
-            return true;
+    while (*password != '\0') { // Loop through each character in the password
+        if (check_range(*password, 49, 58)) { // Check if the character is a number (ASCII values 49-58 correspond to 1-9)
+            return true; // If a number is found, return true
         }
-        ++password;
+        ++password; // Move to the next character
     }
-    return false;
+    return false; 
 }
 
-/* Returns true if the person's first and last name are NOT in the password, false otherwise */
+// Checks if the person's first and last name are not in the password
 bool check_name(const char *first_name, const char *last_name, const char *password) {
-    /* Type "man strstr" in your terminal to learn what strstr does!
-        To exit the man pages, press 'q' */
-    /* Hint: a NULL pointer will evaluate to False in a logical statement while a non-NULL pointer
-        will evaluate to True */
-    const char *first = strstr(password, first_name);
-    const char *last = strstr(password, last_name);
-    return (!(first && last));
+    const char *first = strstr(password, first_name); // Check if the first name is in the password
+    const char *last = strstr(password, last_name); // Check if the last name is in the password
+    return (!(first || last)); // If neither the first nor the last name is in the password, return true
 }
 
-/* Returns true if PASSWORD meets the conditions specified above */
+// Checks if the password meets all the specified conditions
 bool check_password(const char *first_name, const char *last_name, const char *password) {
     bool length, upper, lower, number, name;
-    lower = check_lower(password);
-    assert(lower);
-    length = check_length(password);
-    assert(length);
-    name = check_name(first_name, last_name, password);
-    assert(name);
-    number = check_number(password);
-    assert(number);
-    upper = check_upper(password);
-    assert(upper);
-    return (lower && length && name && upper && number);
+    lower = check_lower(password); 
+    length = check_length(password); 
+    name = check_name(first_name, last_name, password); 
+    number = check_number(password); 
+    upper = check_upper(password); 
+    return (lower && length && name && upper && number); 
 }
+
