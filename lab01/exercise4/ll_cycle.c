67a9355c9dccaa478dfa856d29952e78eebbd84b
bilal7317 <muhammadbilal7317@gmail.com>
2024-03-01 10:49:14 +0500

    Exercise 4 is Done

--- lab01/exercise4/ll_cycle.c
+++ lab01/exercise4/ll_cycle.c
@@ -1,6 +1,30 @@
 #include <stddef.h>
 #include "ll_cycle.h"
+#include <stdbool.h>
 
+// Function to check if a linked list has a cycle
 int ll_has_cycle(node *head) {
-    /* TODO: Implement ll_has_cycle */
+    // If the list is empty, it doesn't have a cycle
+    if (head == NULL) {
+        return false;
+    }
+
+    // Initialize two pointers at the head of the list
+    node *slow_ptr = head;  // This pointer will move one step at a time
+    node *fast_ptr = head;  // This pointer will move two steps at a time
+
+    // Loop through the list
+    while (fast_ptr != NULL && fast_ptr->next != NULL) {
+        slow_ptr = slow_ptr->next;  // Move the slow pointer one step forward
+        fast_ptr = fast_ptr->next->next;  // Move the fast pointer two steps forward
+
+        // If the two pointers meet, the list has a cycle
+        if (slow_ptr == fast_ptr) {
+            return true;
+        }
+    }
+
+    // If the fast pointer reaches the end of the list, the list doesn't have a cycle
+    return false;
 }
+
